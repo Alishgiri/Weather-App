6f4fc69bc772a3a5ab30867995c6b6aa7c9f49d5
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-10 16:56:06 +0545

    Set temperature checkbox group

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,49 @@
 import React from "react";
+import Radio from "@material-ui/core/Radio";
+import Paper from "@material-ui/core/Paper";
 import { inject, observer } from "mobx-react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-import { Col, Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 @inject("weatherStore")
 @observer
 class Home extends React.Component {
+  state = { isCelcius: false };
+
+  setIsCelcius = (isCelcius) => this.setState({ isCelcius });
+
   render() {
     const {
       weatherStore: { weatherData },
     } = this.props;
+    const { isCelcius } = this.state;
     console.log("weatherData", weatherData);
     return (
       <Container>
-        <Col>
-          <h1>Weather App</h1>
-        </Col>
+        <h1 className="mt-3">Weather App</h1>
+        <Paper className="p-3 mt-4" elevation={3}>
+          <Row>
+            <Col className="d-flex justify-content-center">
+              <FormControlLabel
+                value="celcius"
+                label="Celcius"
+                checked={isCelcius}
+                control={<Radio />}
+                onChange={(v) => this.setIsCelcius(true)}
+              />
+            </Col>
+            <Col className="d-flex justify-content-center">
+              <FormControlLabel
+                value="fahrenheit"
+                label="Fahrenheit"
+                checked={!isCelcius}
+                control={<Radio />}
+                onChange={(v) => this.setIsCelcius(false)}
+              />
+            </Col>
+          </Row>
+        </Paper>
       </Container>
     );
   }
