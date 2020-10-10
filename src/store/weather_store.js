b332bb2701a2bd58a87c230f21cf72e32d1881f3
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-10 13:32:46 +0545

    Create mobx store to hold weather data

---
+++ src/store/weather_store.js
@@ -0,0 +1,30 @@
+import Axios from "axios";
+import { action, observable, makeObservable } from "mobx";
+
+import { appId } from "../util/constants";
+
+class WeatherStore {
+  weatherData = null;
+
+  constructor() {
+    makeObservable(this, {
+      weatherData: observable,
+      fetchWeatherData: action,
+    });
+    this.fetchWeatherData();
+  }
+
+  async fetchWeatherData() {
+    try {
+      const res = await Axios.get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=Kathmandu&APPID=${appId}`
+      );
+      this.weatherData = res.data;
+    } catch (e) {
+      console.log("Error", e);
+    }
+  }
+}
+
+const weatherStore = new WeatherStore();
+export default weatherStore;
