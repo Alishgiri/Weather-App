13bf1257c2f953ffc1a397af504ff4924c09754d
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-10 20:49:37 +0545

    Updates

---
+++ precache-manifest.615b2a4d9a0c07396d0e43084e49c817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f13b7f359b1f3a76047d57be1c2ec4",
+    "url": "/Alishgiri/Weather-App/index.html"
+  },
+  {
+    "revision": "8e75d49db541986918c2",
+    "url": "/Alishgiri/Weather-App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "05f40b2c5c9aada3cf3f",
+    "url": "/Alishgiri/Weather-App/static/css/main.fbd2d56f.chunk.css"
+  },
+  {
+    "revision": "8e75d49db541986918c2",
+    "url": "/Alishgiri/Weather-App/static/js/2.9576a880.chunk.js"
+  },
+  {
+    "revision": "082c1bdf24861af2babbd4591bc978fa",
+    "url": "/Alishgiri/Weather-App/static/js/2.9576a880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f40b2c5c9aada3cf3f",
+    "url": "/Alishgiri/Weather-App/static/js/main.24a8543f.chunk.js"
+  },
+  {
+    "revision": "e18ec70a748a0735a546",
+    "url": "/Alishgiri/Weather-App/static/js/runtime-main.2cb8467c.js"
+  }
+]);
