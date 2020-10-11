a2246747f54dc0fbe81bdd5d41ebcaf4c197498c
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-11 17:25:21 +0545

    Upgrade teeko store to handle piece movements

--- src/store/teeko_store.js
+++ src/store/teeko_store.js
@@ -1,25 +1,82 @@
-const { observable, action, makeObservable } = require("mobx");
+import { observable, action, makeObservable } from "mobx";
+
+import { teekoValidMoves } from "../util/teeko_game_constants";
 
 export default class TeekoStore {
-  @observable blocks;
-  @observable redMoves = [];
-  @observable blackMoves = [];
+  isBlackTurn = true;
+  redPlayerIndicator = "#bd172a";
+  blackPlayerIndicator = "#3b3233";
+
+  @observable selectedPiece;
+  @observable.ref redPositions = [];
+  @observable.ref blackPositions = [];
 
   constructor() {
     makeObservable(this);
   }
 
   @action
-  setupBlocks = () => {
-    for (var i = 0; i++; i < 25) {
-      if (this.blocks == null) this.blocks = [];
-      this.blocks.push(i);
+  circleClicked = (position) => {
+    const isRedReserved = this.redPositions.includes(position);
+    const isBlackReserved = this.blackPositions.includes(position);
+
+    const isAllRedPiecesOnBoard = this.redPositions.length === 4;
+    const isAllBlackPiecesOnBoard = this.blackPositions.length === 4;
+
+    if (isRedReserved || isBlackReserved) {
+      if (this.isBlackTurn) {
+        if (isAllBlackPiecesOnBoard && !isRedReserved) {
+          this.selectedPiece = position;
+        } else {
+          alert(`You need to select a "BLACK" piece.`);
+        }
+      } else {
+        if (isAllRedPiecesOnBoard && !isBlackReserved) {
+          this.selectedPiece = position;
+        } else {
+          alert(`You need to select a "RED" piece.`);
+        }
+      }
+      console.log("Selected Piece", this.selectedPiece);
+      return;
     }
+
+    if (this.isBlackTurn) {
+      if (isAllBlackPiecesOnBoard && !this.selectedPiece) {
+        alert(`Please select a "BLACK" piece to move.`);
+        return;
+      }
+      if (
+        isAllBlackPiecesOnBoard &&
+        !teekoValidMoves[this.selectedPiece].includes(position)
+      ) {
+        alert(`Invalid Move!`);
+        return;
+      }
+      if (isAllBlackPiecesOnBoard) {
+        const index = this.blackPositions.indexOf(this.selectedPiece);
+        this.blackPositions.splice(index, 1);
+      }
+      this.blackPositions = [...this.blackPositions, position];
+    } else {
+      if (isAllRedPiecesOnBoard && !this.selectedPiece) {
+        alert(`Please select a "RED" piece to move.`);
+        return;
+      }
+      if (
+        isAllRedPiecesOnBoard &&
+        !teekoValidMoves[this.selectedPiece].includes(position)
+      ) {
+        alert(`Invalid Move!`);
+        return;
+      }
+      if (isAllRedPiecesOnBoard) {
+        const index = this.redPositions.indexOf(this.selectedPiece);
+        this.redPositions.splice(index, 1);
+      }
+      this.redPositions = [...this.redPositions, position];
+    }
+    this.selectedPiece = null;
+    this.isBlackTurn = !this.isBlackTurn;
   };
-
-  @action
-  clickedRed = () => {};
-
-  @action
-  clickedBlack = () => {};
 }
