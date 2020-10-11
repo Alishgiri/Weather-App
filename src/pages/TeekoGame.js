b63144ec123d16dc73c03f9d3f84f4d285f8fe60
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-11 12:12:39 +0545

    Create space for Teeko Game

---
+++ src/pages/TeekoGame.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Col, Container } from "react-bootstrap";
+
+class TeekoGame extends Component {
+  render() {
+    return (
+      <Container className="h-100">
+        <h1 className="mt-3">Teeko Game</h1>
+        <Col className="h-100 d-flex justify-content-center align-items-center">
+          <p>Comming Soon</p>
+        </Col>
+      </Container>
+    );
+  }
+}
+
+export default TeekoGame;
