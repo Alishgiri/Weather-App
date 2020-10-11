7cbf847364875973bcb4020a124e4c9d623c0b32
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-11 18:07:39 +0545

    Add missing Teeko winning streaks

--- src/util/teeko_game_constants.js
+++ src/util/teeko_game_constants.js
@@ -27,11 +27,19 @@ export const teekoBlocks = [
 ];
 
 export const teekoWinningStreaks = [
-  // STRAIGHT
+  // HORIZONTAL
   [1, 2, 3, 4, 5],
   [6, 7, 8, 9, 10],
   [11, 12, 13, 14, 15],
   [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25],
+
+  // VERTICAL
+  [1, 6, 11, 16, 21],
+  [2, 7, 12, 17, 22],
+  [3, 8, 13, 18, 23],
+  [4, 9, 14, 19, 24],
+  [5, 10, 15, 20, 25],
 
   // LEFT TO RIGHT DIAGONAL
   [16, 12, 8, 4],
