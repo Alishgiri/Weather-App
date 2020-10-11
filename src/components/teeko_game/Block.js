623c8b5f92dc51f0c7e218b380a11b83b5003157
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-11 17:25:43 +0545

    Create game board blocks

---
+++ src/components/teeko_game/Block.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  block: {
+    width: 50,
+    height: 50,
+    borderWidth: 2,
+    marginLeft: 2.5,
+    display: "flex",
+    marginRight: 2.5,
+    borderRadius: 25,
+    flexWrap: "wrap",
+    borderStyle: "solid",
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inner_block: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+});
+
+const Block = ({
+  color,
+  onClick,
+  selectedAreaColor,
+  classes: { inner_block, block },
+}) => {
+  return (
+    <div
+      className={block}
+      onClick={onClick}
+      style={{ backgroundColor: selectedAreaColor }}
+    >
+      <div className={inner_block} style={{ backgroundColor: color }}></div>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Block);
