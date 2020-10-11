57d0c258db71e3db6b293aecb2e62d368d89ab92
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-11 08:55:36 +0545

    Correct typo

--- src/util/date_time.js
+++ src/util/date_time.js
@@ -1,11 +1,11 @@
 import moment from "moment";
 
 export function checkIfSameDate(first, second) {
-  const f = convertToCorrectFormate(first);
-  const s = convertToCorrectFormate(second);
+  const f = convertToCorrectFormat(first);
+  const s = convertToCorrectFormat(second);
   return moment(f).isSame(s, "day");
 }
 
-function convertToCorrectFormate(timestamp) {
+function convertToCorrectFormat(timestamp) {
   return moment(timestamp * 1000).format("YYYY-MM-DD");
 }
