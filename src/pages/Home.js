acfaac05609fee3cf0e2c076abe18c94073be692
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-23 14:28:15 +0545

    Align home screen title properly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Col, Container } from "react-bootstrap";
 const Home = () => {
   return (
     <Container className="h-100" fluid>
-      <h1 className="mt-3">Home</h1>
+      <h1 className="mt-3 ml-4">Home</h1>
       <Col className="d-flex justify-content-center align-items-center">
         <p>Experimental playgrounds for React app features.</p>
       </Col>
