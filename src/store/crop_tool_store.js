c34ebf83fd09f4e5da920e7bc0f492896218f3e8
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-23 11:40:29 +0545

    Add download functionality to crop tool store

--- src/store/crop_tool_store.js
+++ src/store/crop_tool_store.js
@@ -1,9 +1,15 @@
 const { makeObservable, observable, action } = require("mobx");
 
-const { image64toCanvasRef } = require("../util/images_and_files");
+const {
+  getCroppedImg,
+  downloadBase64File,
+  extractImageFileExtensionFromBase64,
+} = require("../util/images_and_files");
 
 export default class CropToolStore {
   @observable imageSrc;
+  @observable imageRef;
+  @observable croppedBase64ImageSrc;
   @observable.ref crop = { aspect: 16 / 9 };
 
   constructor() {
@@ -12,30 +18,45 @@ export default class CropToolStore {
 
   @action
   onDrop = (acceptedFiles) => {
+    this.croppedBase64ImageSrc = null;
     const file = acceptedFiles[0];
-    // this.imageSrc = URL.createObjectURL(file);
-    // console.log({ file });
-    const reader = new FileReader();
-    reader.addEventListener(
-      "load",
-      () => this.setImageSrc(reader.result),
-      false
-    );
-    reader.readAsDataURL(file);
+
+    // TO GET IMAGE SRC USE THIS
+    this.imageSrc = URL.createObjectURL(file);
+    // OR
+    // const reader = new FileReader();
+    // reader.addEventListener(
+    //   "load",
+    //   () => this.setImageSrc(reader.result),
+    //   false
+    // );
+    // reader.readAsDataURL(file);
   };
 
-  @action
-  setImageSrc = (src) => (this.imageSrc = src);
+  // @action
+  // setImageSrc = (src) => (this.imageSrc = src);
 
   @action
   handleCropChange = (crop, percentCrop) => (this.crop = crop);
 
-  handleCropComplete = (crop, percentCrop, canvas) => {
+  @action
+  setcroppedBase64ImageSrc = (src) => (this.croppedBase64ImageSrc = src);
+
+  @action
+  handleImageLoaded = (image) => (this.imageRef = image);
+
+  handleCropComplete = async (crop, percentCrop, canvasRef) => {
     console.log({ percentCrop, crop, "this.imageSrc": this.imageSrc });
-    image64toCanvasRef(canvas, this.imageSrc, crop);
+    const base64Image = await getCroppedImg(this.imageRef, crop, canvasRef);
+    this.setcroppedBase64ImageSrc(base64Image);
   };
 
-  handleImageLoaded = (imageLoaded) => {
-    console.log({ imageLoaded });
+  downloadCroppedImage = (event) => {
+    event.preventDefault();
+    const fileExtension = extractImageFileExtensionFromBase64(
+      this.croppedBase64ImageSrc
+    );
+    const fullFileName = "untitled." + fileExtension;
+    downloadBase64File(this.croppedBase64ImageSrc, fullFileName);
   };
 }
