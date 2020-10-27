36610f1f0ac86242f833713ff853bc8d4e5a7d60
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2020-10-27 19:16:32 +0545

    Updates

--- precache-manifest.279e3317c232c4f5c1db4898c2cae55b.js
+++ precache-manifest.279e3317c232c4f5c1db4898c2cae55b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d64dbc5e63565e5bba9cc4055e3a3eb",
+    "revision": "2f45e134b4c4b4593689d55df5616cb9",
     "url": "/React-Advanced-Playground/index.html"
   },
   {
-    "revision": "8bee02bd4a6171ced947",
+    "revision": "5b64f9433a467bba9725",
     "url": "/React-Advanced-Playground/static/css/2.46310fc8.chunk.css"
   },
   {
-    "revision": "3ecfeb84dc8c32505fd6",
+    "revision": "b1ac69d0bfa7d467933c",
     "url": "/React-Advanced-Playground/static/css/main.bbce343e.chunk.css"
   },
   {
-    "revision": "8bee02bd4a6171ced947",
-    "url": "/React-Advanced-Playground/static/js/2.4691a34d.chunk.js"
+    "revision": "5b64f9433a467bba9725",
+    "url": "/React-Advanced-Playground/static/js/2.f1aaad4d.chunk.js"
   },
   {
     "revision": "2321fb33f83f11ac83cbb97d1f2ba6cd",
-    "url": "/React-Advanced-Playground/static/js/2.4691a34d.chunk.js.LICENSE.txt"
+    "url": "/React-Advanced-Playground/static/js/2.f1aaad4d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3ecfeb84dc8c32505fd6",
-    "url": "/React-Advanced-Playground/static/js/main.630aab30.chunk.js"
+    "revision": "b1ac69d0bfa7d467933c",
+    "url": "/React-Advanced-Playground/static/js/main.75c9c545.chunk.js"
   },
   {
     "revision": "44d980f5d10c6dd05874",
