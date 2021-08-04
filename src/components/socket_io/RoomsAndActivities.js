f0a86de0fcf8c05426e2466e665ee218ba63a4ca
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-08-04 19:23:45 +0545

    Rename RoomsSpace to RoomsAndActivities

--- src/components/socket_io/RoomsAndActivities.js
+++ src/components/socket_io/RoomsAndActivities.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Col, Button } from "react-bootstrap";
 
-const RoomsSpace = ({ onChange }) => {
+const RoomsAndActivities = ({ onChange, onPressJoin }) => {
   return (
     <Col>
-      <h5>Select or Create a room</h5>
+      <h5>Join a room</h5>
       <div className="input-group">
         <input
           type="text"
           onChange={onChange}
           className="form-control"
-          placeholder="Create new room…"
+          placeholder="type room name…"
         />
-        <Button class="my-2 btn btn-outline-primary">Create</Button>
+        <Button onClick={onPressJoin}>Join</Button>
       </div>
     </Col>
   );
 };
 
-export default RoomsSpace;
+export default RoomsAndActivities;
