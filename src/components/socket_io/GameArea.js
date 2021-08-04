1c81ad406c9db837ad1acf799e06b14392f843f9
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2021-08-04 23:04:26 +0545

    Finish basic game logic in,
    - GameArea
    - RoomsAndActivities

--- src/components/socket_io/GameArea.js
+++ src/components/socket_io/GameArea.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Col, Row, Button } from "react-bootstrap";
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import ArrowUpIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownIcon from "@material-ui/icons/ArrowDownward";
 
-const GameArea = ({ onPressBack, onPressForward }) => {
+const GameArea = ({ players, onPressUpward, onPressDownward }) => {
   return (
     <Col className="d-flex flex-column">
-      <div className="bg-dark m-4 rounded d-flex flex-grow-1"></div>
+      <div className="bg-dark ml-2 my-2 border rounded d-flex flex-grow-1 position-relative">
+        {players &&
+          players.map((p, index) => (
+            <div
+              key={`${p.userColor}:${index}`}
+              style={{
+                bottom: p.move,
+                left: (index + 1) * 40,
+                backgroundColor: p.userColor,
+              }}
+              className="p-2 rounded position-absolute"
+            />
+          ))}
+      </div>
       <Row className="mx-5 align-items-center justify-content-between">
-        <Button className="mr-3" onClick={onPressBack}>
-          <ArrowBackIcon />
+        <Button className="mr-3" onClick={onPressUpward}>
+          <ArrowUpIcon />
         </Button>
-        <h5>Players: 0</h5>
-        <Button className="ml-3" onClick={onPressForward}>
-          <ArrowForwardIcon />
+        <h5>Players: {players.length}</h5>
+        <Button className="ml-3" onClick={onPressDownward}>
+          <ArrowDownIcon />
         </Button>
       </Row>
     </Col>
--- src/components/socket_io/RoomsAndActivities.js
+++ src/components/socket_io/RoomsAndActivities.js
@@ -1,19 +1,44 @@
 import React from "react";
 import { Col, Button } from "react-bootstrap";
 
-const RoomsAndActivities = ({ onChange, onPressJoin }) => {
+const RoomsAndActivities = ({
+  activities,
+  onPressJoin,
+  onPressDone,
+  onChangeRoomName,
+  onChangeYourColor,
+}) => {
   return (
     <Col>
       <h5>Join a room</h5>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          onChange={onChangeYourColor}
+          placeholder="Type Your Color"
+        />
+        <Button onClick={onPressDone}>Done!</Button>
+      </div>
       <div className="input-group">
         <input
           type="text"
-          onChange={onChange}
           className="form-control"
+          onChange={onChangeRoomName}
           placeholder="type room name…"
         />
         <Button onClick={onPressJoin}>Join</Button>
       </div>
+      {activities &&
+        activities.length > 0 &&
+        activities.map((activity, index) => (
+          <h6
+            key={index}
+            className="mt-3 p-2 shadow rounded bg-primary text-light"
+          >
+            {activity}
+          </h6>
+        ))}
     </Col>
   );
 };
