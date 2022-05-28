0fb2b1f25ecbf52335f45235dfabd6615376ed39
Alish Giri <32293725+Alishgiri@users.noreply.github.com>
2022-05-28 11:55:50 +0545

    Create page to practice react hooks

---
+++ src/pages/react_hooks/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const ReactHooks = () => {
+  return (
+    <Container className="h-100" fluid>
+      <h1>React Hooks</h1>
+    </Container>
+  );
+};
+
+export default ReactHooks;
